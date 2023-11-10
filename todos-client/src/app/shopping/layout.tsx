23b1c619005ec7bb2984e89b50af16e65e7c92ca
Paulo Wells <paulowells@gmail.com>
2023-11-10 00:07:57 -0300

    started branch shopping

---
+++ todos-client/src/app/shopping/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout (props: {
+    children: React.ReactNode,
+}) {
+    return (
+        <div className="shopping">
+            {props.children}
+        </div>
+    );
+}
